8c650eab38189047c5c4c64ca319213124e3569c
Gregwar <g.passault@gmail.com>
2014-12-20 08:44:19 +0100

    Comment tex2png

--- demo/tex2png.php
+++ demo/tex2png.php
@@ -4,6 +4,15 @@ require('../autoload.php');
 
 use Gregwar\Tex2png\Tex2png;
 
+/**
+ * This is a simple command-line tool using the lib
+ *
+ * One can invoke it with:
+ * php tex2png.php "\\sum_{i=1}^{i=n} i^2" out.png
+ *
+ * This will output the given formula to out.png
+ */
+
 $args = $_SERVER['argv'];
 
 if (count($args) != 3) {
