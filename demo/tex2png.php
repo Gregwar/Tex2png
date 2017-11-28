522aff8c1fc463d564619225d799b89bac5a5a15
Gregwar <g.passault@gmail.com>
2017-11-28 18:16:16 +0100

    Adding tex2png script using stdin

--- demo/tex2png.php
+++ demo/tex2png.php
@@ -1,3 +1,4 @@
+#!/usr/bin/env php
 <?php
 
 require('../autoload.php');
@@ -8,23 +9,25 @@ use Gregwar\Tex2png\Tex2png;
  * This is a simple command-line tool using the lib
  *
  * One can invoke it with:
- * php tex2png.php "\\sum_{i=1}^{i=n} i^2" out.png
+ * php tex2png.php out.png
  *
- * This will output the given formula to out.png
+ * And type the forumla on stdin (ctrl+D to end)
+ *
+ * This will output the given to out.png, and input on stdin
  */
 
 $args = $_SERVER['argv'];
 
-if (count($args) != 3) {
+if (count($args) != 2) {
     $stderr = fopen('php://stderr', 'w');
-    fprintf($stderr, "Usage: php tex2png.php formula output.png\n");
+    fprintf($stderr, "Usage: php tex2png.php output.png\n");
 } else {
-    $formula = $args[1];
-    $output = $args[2];
+    $formula = trim(file_get_contents('php://stdin'));
+    $output = $args[1];
 
     echo "Generating $output...\n";
 
-    Tex2png::create($formula)
+    Tex2png::create($formula, 500)
         ->saveTo($output)
         ->generate();
 }
